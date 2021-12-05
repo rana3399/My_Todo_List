4b07013f2282eee9fb01e0bc103009848b2922de
Rana_Ahmed <foster3399@gmail.com>
2021-12-05 11:41:09 +0100

    fixing page  refresh issue

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import './App.css';
 import Todo from './Todo';
 import Home from './Home';
 
-import { BrowserRouter,Routes, Route, Link } from "react-router-dom";
+import { HashRouter,Routes, Route, Link } from "react-router-dom";
 
 
 function App() {
  
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/todo" element={<Todo/>}  />
         </Routes>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
